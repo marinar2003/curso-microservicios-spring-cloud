78d6946d525ee598ca016d42066eff9f95943db6
Marina <mnrevol50@gmail.com>
2026-02-01 12:38:16 -0300

    configuracion Circuit Breaker

--- order-service/order-service/src/main/java/com/example/order_service/controller/OrderController.java
+++ order-service/order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.order_service.controller;
 
 import com.example.order_service.client.InventoryClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +22,15 @@ public class OrderController {
     //return "Pedido recibido. Estado del inventario: " + status;
 
     @GetMapping("/create")
+    @CircuitBreaker(name = "inventoryCB", fallbackMethod = "fallbackInventory")
     public String createOrder(@RequestParam("code") String code) { // Agregamos el RequestParam
         // Ahora usamos el 'code' que viene de Postman
         String status = inventoryClient.isInStock(code);
         return "Pedido recibido. Producto: " + code + ". Estado del inventario: " + status;
+    }
+
+    //Este método se ejecuta cuando el circuito está ABIERTO o hay error
+    public String fallbackInventory(String code, Throwable throwable) {
+        return "Lo sentimos, el servicio de inventario no está disponible para el producto: " + code + ". Intente más tarde.";
     }
 }
