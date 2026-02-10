0cfb0274fb057c98172ed6d2b6ebd5c7a394f5ae
Marina <mnrevol50@gmail.com>
2026-02-09 22:18:43 -0300

    implemento oauth2 security en api-gateway usando keycloak, creo clase SecurityCongif en api-gateway

---
+++ api-gateway/api-gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated() // Todo debe estar autenticado
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()) // Usamos tokens JWT
+                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable); // Desactivamos CSRF para pruebas
+
+        return http.build();
+    }
+}
